60fb2bbed1f6c9c1a52e4f7e31d17e81c13e6d75
LDGroenenberg <groe0087@hz.nl>
2025-02-15 15:54:34 +0100

    added the home page

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,20 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/sass/app.scss', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+                //styles
+                'resources/css/indexstyles.css',
+                'resources/css/header.css',
+                'resources/css/footer.css',
+                //images
+                'resources/images/backgroundimage2.png',
+                'resources/images/githubicon.png',
+                'resources/images/hz-logo.png',
+                'resources/images/hz-university-of-applied-sciences.png',
+                'resources/images/githubicon.png',
+            ],
             refresh: true,
         }),
     ],
