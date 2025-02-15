72455791caf4517de1996a62ba4436121c7d7f06
LDGroenenberg <groe0087@hz.nl>
2025-02-15 19:59:34 +0100

    added the faq page

---
+++ resources/views/faq.blade.php
@@ -0,0 +1,72 @@
+@extends('layout.layout')
+
+@section('contentcss')
+    <link rel="stylesheet" href="{{ Vite::asset('resources/css/faqstyles.css') }}">
+@endsection
+
+@section('content')
+    <details>
+        <summary>&lt;How can you print a document from your laptop at HZ/&gt;</summary>
+        <p>
+            1. Log on to the printer using your HZ card or by entering your HZ user name & password.<br/>
+            2. Choose the Scan option in the menu.<br/>
+            3. Place the original(s) on the feeder or glass plate.<br/>
+            - Double-sided scanning (optional)<br/>
+            4. Press Change settings.<br/>
+            5. For duplex mode, select the 2-sided option.<br/>
+            6. Press Start.<br/>
+            7. After scanning, press the following symbol on the printer to log off.<br/>
+            You can find the instuctions at the hz <a href="https://hzuniversity.topdesk.net/tas/public/ssp/content/detail/knowledgeitem?origin=knowledgeBaseTop10&unid=d065a4e8a56d47dcbd0ffb9cdf78f20b&from=fd2e43e7-c973-4b08-8bbd-bcee9b50ea22" target="_blank">FAQ</a> page
+        </p>
+    </details>
+    <details>
+        <summary>&lt;How can you scan a document and send it to your laptop at HZ/&gt;</summary>
+        <p>
+            1. Log on to the printer using your HZ card or by entering your HZ user name & password.<br/>
+            2. Choose the Scan option in the menu.<br/>
+            3. Place the original(s) on the feeder or glass plate.<br/>
+            3. Double-sided scanning (optional)<br/>
+            • Press Change settings<br/>
+            • For duplex mode, select the 2-sided option<br/>
+            5. Press Start<br/>
+            6. After scanning, press the following symbol on the printer to log off<br/>
+            You can find the instuctions at the hz <a href="https://hzuniversity.topdesk.net/tas/public/ssp/content/detail/knowledgeitem?origin=knowledgeBaseTop10&unid=d065a4e8a56d47dcbd0ffb9cdf78f20b&from=fd2e43e7-c973-4b08-8bbd-bcee9b50ea22" target="_blank">FAQ</a> page
+        </p>
+    </details>
+    <details>
+        <summary>&lt;How can I buy something (like when I sign up for the IT introduction event) on the HZ web shop/&gt;</summary>
+        <p>
+            1. go to your myhz page.<br/>
+            2. within the personal menu navagate to the webshop.<br/>
+            3. from there you can select the item you want.<br/>
+            4. you can purchase your item.<br/>
+            You can find the instuctions at the hz <a href="https://hzuniversity.topdesk.net/tas/public/ssp/content/detail/knowledgeitem?unid=52c10671036e4bee82751bbde98585eb" target="_blank">FAQ</a> page
+        </p>
+    </details>
+    <details>
+        <summary>&lt;How can you book a project space/&gt;</summary>
+        <p>
+            1. Go to the home page of the Self Service Portal<br/>
+            2. Click on Reservations<br/>
+            3. Choose the appropriate category and search for the "resource" you want to reserve.<br/>
+            If necessary, use the Type filter to turn certain items on/off in the overview.<br/>
+            4. Select the day when the resource is borrowed.<br/>
+            5. By dragging the left mouse button, indicate the time period when the resource is borrowed.<br/>
+            6. Click Next.<br/>
+            7. Read the loan conditions, check the reservation and click Next.<br/>
+            8. Fill in the necessary information and click Submit request.<br/>
+            You can find the instuctions at the hz <a href="https://hzuniversity.topdesk.net/tas/public/ssp/content/detail/knowledgeitem?origin=knowledgeBaseTop10&unid=222efc2f5baa4a5ba75647c7bec4482a&from=fd2e43e7-c973-4b08-8bbd-bcee9b50ea22" target="_blank">FAQ</a> page
+        </p>
+    </details>
+    <details>
+        <summary>&lt;What are the instructions if you want to park your car at the HZ parking lot/&gt;</summary>
+        <p>
+            It is not possible to park directly at the HZ on Het Groene Woud.<br/>
+            There is 1 special parking space available on the grounds for people with disabilities and/or chronic conditions.<br/>
+            <br/>
+            The car park on Kousteensedijk has a number of spaces for visitors, staff and students of Het Groene Woud.<br/>
+            This car park is an 8-minute walk from the HZ.<br/>
+            Upon showing your entry card, you can then get an exit card at the JRCZ service desk.
+        </p>
+    </details>
+@endsection
